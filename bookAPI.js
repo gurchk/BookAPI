ba3e76a23140ff96012a767f3ca44ca88af4f5e5
Anton Stjernquist <antonstjernquist@gmail.com>
2017-11-22 14:55:37 +0100

    Added functions; saveKey,retrieveKey and addBook.

--- bookAPI.js
+++ bookAPI.js
@@ -16,5 +16,46 @@ window.addEventListener('load', function (event) {
         requestedAPI.open('GET', `https://www.forverkliga.se/JavaScript/api/crud.php?requestKey`)
         requestedAPI.send();
     });
-    
+
+      /* Add eventListener for saveKey button */
+      document.getElementById('useApiKey').addEventListener('click', saveKey);
+
+      /* Testing addBook */
+      addBook('hi', 'bye');
+
 });
+
+/* Functions */
+
+function saveKey(){
+
+  /* Declare constant apiVariable */
+  const apiKeyValue = document.getElementById('apiKEY').innerText;
+
+  /* Save the key to local storage */
+  localStorage.setItem('apiKey', apiKeyValue);
+
+
+}
+
+function retrieveKey(){
+
+  /* Retrieve apiKey */
+  return localStorage.getItem('apiKey');
+}
+
+function addBook(title, author){
+
+      const addBookRequest = new XMLHttpRequest();
+      let responseText = null;
+      addBookRequest.onreadystatechange = function(event) {
+          if (addBookRequest.readyState === 4) {
+              responseText = JSON.parse(addBookRequest.responseText);
+              console.log(responseText.status);
+              console.log(responseText.id);
+          }
+      }
+      
+      addBookRequest.open('GET', `https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=${retrieveKey()}&title=${title}&author=${author}`);
+      addBookRequest.send();
+}
