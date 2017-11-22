7d047a61466e8c4c536be77070df0cbb156c962a
Anton Stjernquist <antonstjernquist@gmail.com>
2017-11-22 13:32:11 +0100

    Intenderade lite i bookAPI.js

--- bookAPI.js
+++ bookAPI.js
@@ -1,10 +1,12 @@
 window.addEventListener('load', function (event) {
+
     const requestAPIbtn = document.getElementById('requestAPI');
+
     requestAPIbtn.addEventListener('click', function (event) {
         const requestedAPI = new XMLHttpRequest();
         const log = document.getElementById('apiKEY');
         let ourNiceKey = null;
-        
+
         requestedAPI.onreadystatechange = function(event) {
             if (requestedAPI.readyState === 4) {
                 ourNiceKey = JSON.parse(requestedAPI.responseText);
@@ -13,6 +15,6 @@ window.addEventListener('load', function (event) {
         }
         requestedAPI.open('GET', `https://www.forverkliga.se/JavaScript/api/crud.php?requestKey`)
         requestedAPI.send();
-    })
+    });
     
-})
+});
